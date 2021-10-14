5a57d94cdacaa689281182b1d9cc01491e918664
soundtrack <yarom750@gmail.com>
2021-10-14 18:19:48 +0300

    created fixed length

--- pybuster.py
+++ pybuster.py
@@ -15,9 +15,42 @@ def extensions(url, wordlist, extension):
     pass
 
 
-def fixed_length(url, wordlist, size):
+def fixed_length(url, wordlist, se, size):
     urllib3.disable_warnings()
-    pass
+    with open(wordlist, 'r') as wl:
+        while True:
+            for w in wl.readlines():
+                w = w.replace('\n', '')
+                # print("test: ", url + w)
+                try:
+                    req = se.get(url + w, allow_redirects=True, verify=False, timeout=0.5)
+                except Exception:
+                    req = se.get(url + w, allow_redirects=True, verify=False)
+                # print(f"started a new thread, testing url: {req.url}")
+                if req.status_code in range(400, 499):
+                    if "Access" in req.text or "Forbidden" in req.text:
+                        print(f"[-] Found /{w}  directory but you don't have access to it")
+                    else:
+                        # print(line)
+                        continue
+
+                elif req.status_code in range(200, 299) or req.status_code in range(300, 399) \
+                        and len(req.content > {size}):
+                    urlc = url.count('/') + w.count('/')
+                    print(f"[+] Found directory /{w} -> [{req.url} | {len(req.content)}]")
+
+                    if urlc <= 4:
+                        pass
+                        # t = Thread(target=new_thread, args=(url + w + '/', s, wordlist))
+                        # t.daemon = True
+                        # t.start()
+                        # os.system(f"""terminator -T 'dir {line} '--new-tab -x 'python3 /home/$USER/pybuster/pybuster.py {url + line}/ {wordlist};echo "\n\033[1;33mPress ENTER to continue";read'""")
+                        # Need to add status bar and a command that opens a new tab with a title of the dir found.
+                    else:
+                        continue
+                else:
+                    continue
+            break
 
 
 def new_thread(url, se, wlt):
@@ -28,12 +61,12 @@ def new_thread(url, se, wlt):
                 w = w.replace('\n', '')
                 # print("test: ", url + w)
                 try:
-                    req = se.get(url + line, allow_redirects=True, verify=False, timeout=0.5)
+                    req = se.get(url + w, allow_redirects=True, verify=False, timeout=0.5)
                 except Exception:
-                    req = se.get(url + line, allow_redirects=True, verify=False)
+                    req = se.get(url + w, allow_redirects=True, verify=False)
                 # print(f"started a new thread, testing url: {req.url}")
                 if req.status_code in range(400, 499):
-                    if "Access Denied" in req.text or "Forbidden" in req.text:
+                    if "Access" in req.text or "Forbidden" in req.text:
                         print(f"[-] Found /{w}  directory but you don't have access to it")
                     else:
                         # print(line)
@@ -75,7 +108,7 @@ def main(url, wordlist, session):
                         r = session.get(url + line, allow_redirects=True, verify=False)
 
                     if r.status_code in range(400, 499):
-                        if "Access Denied" in r.text or "Forbidden" in r.text:
+                        if "Access" in r.text or "Forbidden" in r.text:
                             print(f"[-] Found /{line}  directory but you don't have access to it")
                         else:
                             # print(line)
@@ -108,28 +141,29 @@ def main(url, wordlist, session):
 
 
 if __name__ == '__main__':
-    # Insted of opening multitabs I need can create a thred that go recursive in the same search
     # Need to add if the length of the page is the same and has 200 code then to leave it.
     # Need to add an option for fix length like: if the 404(200 code) page's length is 450 then do the command below:
     # if r.status_code in range(200, 299) and len(r.content > {user_fixed_size})
 
     # Need to add option for extensions -x php,html,sh.
 
-    # Need to add full recursive(No restrict)
+    # Need to add full recursive(No restrict) -f
 
     # Need to add an option to follow recursive on 404 access denied or Forbidden. -F?
-
-    # Need to add -n for no-tab(Not going recursive)
 
     # parser = argparse.ArgumentParser()
     # parser.parse_args()
 
-    url = "http://127.0.0.1/"
-    wordlist = "/home/soundtrack/Desktop/word.txt"
+    # url = "http://127.0.0.1/"
+    # wordlist = "/home/soundtrack/Desktop/word.txt"
 
-    # url = sys.argv[1]
-    # wordlist = sys.argv[2]
+    url = sys.argv[1]
+    wordlist = sys.argv[2]
+
+    length = sys.argv[3]
 
     s = requests.Session()
 
-    main(url, wordlist, s)
+    fixed_length(url, wordlist, s, length)
+
+    # main(url, wordlist, s)
