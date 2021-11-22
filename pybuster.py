22f8d9b97bb575cea895578c42848171b0253ee4
soundtrack505 <yarom750@gmail.com>
2021-11-22 22:44:59 +0200

    Work in progress

--- pybuster.py
+++ pybuster.py
@@ -2,7 +2,7 @@
 
 """
 Created by: Yerom Hemo
-Point: I was sick from gobuster and dirb no going recursive as soon as they find a hit so I created this program
+Point: I was sick from gobuster and dirb not going recursive as soon as they find a hit so I created this program
 I will try to make it faster in the future.
 """
 
